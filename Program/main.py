4fbd23c051e48e9c61598f79ce10df07eb48b72e
Bagoes-Heikhal <bagoesbram223@gmail.com>
2021-05-23 22:15:22 +0700

    seperating question & answer also cleaning the text

--- Program/main.py
+++ Program/main.py
@@ -23,11 +23,36 @@ for line in lines:
         id2line[_line[0]] = _line[4]
 
 # conversation data cleaning
-
 conversation_ids = []
 
 for conversation in conversations[:-1]:
     _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
     conversation_ids.append(_conversation.split(","))
 
-print(len(conversation_ids[0]))
+# seperate answer and question
+# question text adalah 1 text sebelum pertanyaan
+questions = []
+answer = []
+
+for conversation in conversation_ids:
+    for i in range(len(conversation)-1):
+        questions.append(id2line[conversation[i]])
+        answer.append(id2line[conversation[i+1]])
+
+#cleaning the text, to make it easier to train
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
+    return text
